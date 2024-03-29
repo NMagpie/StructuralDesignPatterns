5755a203034bd19265140adcd8acdc9422a31a1d
NMagpie <sorochin.work@gmail.com>
2024-03-29 15:44:02 +0200

    SLIGHT REFACTOR FOR MAIN FILE

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using _16._Structural_Design_Patterns.Formatting.ConcreteDecorators;
+using System.Runtime.Serialization;
 
 var textDecorator = new FormattingDecorator("Hello, world!");
 
 var boldDecorator = new BoldFormattingDecorator(textDecorator);
 
 var colorDecorator = new ColorFormattingDecorator(boldDecorator, "yellow");
+
+Console.WriteLine(colorDecorator.FormatText());
+
+colorDecorator.RemoveChild(new BoldFormattingDecorator(null));
 
 Console.WriteLine(colorDecorator.FormatText());
